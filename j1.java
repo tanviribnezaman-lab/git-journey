95923bd3b79c20a884689c64e793f73a631f2344
tanviribnezaman-lab <tanviribnezaman@iut-dhaka.edu>
2026-04-29 20:12:43 +0600

    Add files via upload

---
+++ j1.java
@@ -0,0 +1,11 @@
+public class J1 {
+    public static void main(String[] args) {
+        String car ="Ford";
+        Boolean isFast= false;
+
+        if (isFast)) {
+            
+        }
+    }
+    
+}
